5e4978c0954930679fbf56f069006e8df585a569
timweissenfels <timweissenfels7@gmail.com>
2021-08-03 20:54:51 +0200

    Sample implementation with 128bit and time tracking

--- main.cpp
+++ main.cpp
@@ -2,9 +2,11 @@
 
 #include <iostream>
 #include <fstream>
+#include <chrono>
 #include <boost/multiprecision/cpp_int.hpp>
 #include <boost/random/mersenne_twister.hpp>
 #include <boost/random/uniform_int_distribution.hpp>
+#include <boost/multiprecision/miller_rabin.hpp>
 
 using namespace boost::multiprecision;
 
@@ -12,7 +14,7 @@ using namespace boost::multiprecision;
 1.Generate a random 512 bit odd number, say p
 
 2. Test to see if p is prime; 
-if it is, return p; this is expected to occur after testing about Log(p)/2∼177 candidates
+if it is, return p; this is expected to occur after testing about Log(p)/2 ∼177 candidates
 
 3. Otherwise set p=p+2, goto 2
 */
@@ -21,6 +23,35 @@ template <class T>
 T generate_number(auto &random_generator, T max) {
     boost::random::uniform_int_distribution<T> dist(1, max);
     return dist(random_generator);
+}
+
+template <class T>
+bool isPrime(T numb)
+{
+    T it;
+    for (it = 2; it < numb; it++)
+    {
+        if ((numb % it) == 0) {
+            std::cout << "Non" << std::endl;
+            return false;
+        }
+    }
+
+    return true;
+}
+
+template <class T>
+bool check_prime(T num) {
+   if(num % 2 == 0)
+       return false;
+
+   boost::mt11213b gen2(clock());
+
+   for(unsigned i = 0; i < 100000; ++i) {
+      if(miller_rabin_test(num, 25, gen2)) 
+        return true;
+   }
+   return false;
 }
 
 // Taken from https://stackoverflow.com/a/1900161/8964221
@@ -35,13 +66,22 @@ int getSeed()
 }
 
 int main() {
-    uint512_t max_num = (std::numeric_limits<uint512_t>::max)();
+    uint128_t max_num = (std::numeric_limits<uint128_t>::max)();
+    auto t1 = std::chrono::high_resolution_clock::now(); 
     
-    //Get Random 512 Bit odd Number
-    boost::mt19937 random_generator(getSeed());
-    uint512_t rand_num = 0;
-    while(rand_num % 2 == 0) { rand_num = generate_number(random_generator,max_num);}
+    //Get Random 128 Bit odd Number
+    boost::mt19937 random_generator(clock());
+    auto rand_num = generate_number(random_generator,max_num);
+   
+    while(!check_prime(rand_num)) { 
+        rand_num = generate_number(random_generator,max_num);
+        std::cout << rand_num << std::endl;
+    }
 
-    std::cout << rand_num << std::endl; 
+    std::cout << "FOUND: " << rand_num << std::endl;
+    
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::seconds>( t2 - t1 ).count();
+    std::cout << duration << std::endl;
     return 0;
 }
