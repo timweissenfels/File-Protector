27be96d0fb558b5e80bcab0a89de20676bef84dd
timweissenfels <timweissenfels7@gmail.com>
2021-08-10 20:37:36 +0200

    Added compiler flags for Google Benchmarks

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-g++ main.cpp -pthread -O2 -o File-Protector  -Wfatal-errors -I ./boost
+g++ main.cpp -pthread -lbenchmark  -isystem benchmark/include -Lbenchmark/build/src -O2 -o File-Protector  -Wfatal-errors -I ./boost
 ./File-Protector
