9e991e39694a597841af2360894053412847398a
timweissenfels <timweissenfels7@gmail.com>
2021-08-10 20:41:12 +0200

    Changed filenames, now using undescore instead of dashes

---
+++ build.sh
@@ -0,0 +1,1 @@
+g++ main.cpp -pthread -lbenchmark  -isystem benchmark/include -Lbenchmark/build/src -O2 -o File_Protector -Wfatal-errors -I ./boost
--- start.sh
+++
@@ -1,2 +0,0 @@
-g++ main.cpp -pthread -lbenchmark  -isystem benchmark/include -Lbenchmark/build/src -O2 -o File-Protector  -Wfatal-errors -I ./boost
-./File-Protector
