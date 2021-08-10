e64a262b57ee5f3741ac55d94df02d750e161fdf
timweissenfels <timweissenfels7@gmail.com>
2021-08-10 20:37:02 +0200

    Added Script to setup Google Benchmark framework / makes github workflows easy

---
+++ setup-google-benchmark.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/google/benchmark.git
+git clone https://github.com/google/googletest.git benchmark/googletest
+cd benchmark
+cmake -E make_directory "build"
+cmake -E chdir "build" cmake -DCMAKE_BUILD_TYPE=Release ../
+cmake --build "build" --config Release
+
