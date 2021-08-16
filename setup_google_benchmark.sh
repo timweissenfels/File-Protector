669fbc7cfd6cbc00916a003777b47ba4124b1b8e
timweissenfels <timweissenfels7@gmail.com>
2021-08-17 01:27:56 +0200

    Updated Paths and installer script

--- setup_google_benchmark.sh
+++ setup_google_benchmark.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-git clone https://github.com/google/benchmark.git
-git clone https://github.com/google/googletest.git benchmark/googletest
 cd benchmark
 cmake -E make_directory "build"
 cmake -E chdir "build" cmake -DCMAKE_BUILD_TYPE=Release ../
