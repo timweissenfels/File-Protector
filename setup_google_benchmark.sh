5a36f9fbc99b8252adeed0fc0fb65048ca662313
timweissenfels <timweissenfels7@gmail.com>
2021-08-17 02:04:12 +0200

    Fixes bug with git submodules in same folder

--- setup_google_benchmark.sh
+++ setup_google_benchmark.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+git clone https://github.com/google/googletest.git /benchmark/googletest
 cd benchmark
 cmake -E make_directory "build"
 cmake -E chdir "build" cmake -DCMAKE_BUILD_TYPE=Release ../
