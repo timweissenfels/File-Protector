a4cde031e4ec18b1cef8777f74101fdbdb423177
timweissenfels <timweissenfels7@gmail.com>
2022-08-08 19:06:34 +0200

    Made Output more readable

--- main.cpp
+++ main.cpp
@@ -11,8 +11,8 @@ int main() {
     cpp_int totient = get_totient(primes);
     cpp_int n_value = get_n_value(primes);
 
-    std::cout << primes.first << std::endl << primes.second << std::endl;
-    std::cout << totient << std::endl;
-    std::cout << n_value << std::endl;
+    std::cout << primes.first << std::endl << primes.second << std::endl << std::endl;
+    std::cout << totient << std::endl << std::endl;
+    std::cout << n_value << std::endl << std::endl;
 
 }
