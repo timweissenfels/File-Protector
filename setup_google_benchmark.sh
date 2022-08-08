d11dd3f81dd468fa18c730802432da5e34633400
timweissenfels <timweissenfels7@gmail.com>
2022-08-08 19:16:23 +0200

    Changed build path

--- setup_google_benchmark.sh
+++ setup_google_benchmark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git clone https://github.com/google/googletest.git benchmark/googletest
-cd benchmark
+cd benchmark/googletest
 cmake -E make_directory "build"
 cmake -E chdir "build" cmake -DCMAKE_BUILD_TYPE=Release ../
 cmake --build "build" --config Release
